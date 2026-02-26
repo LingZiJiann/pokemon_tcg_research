b4f59f541f38e3008e1ec475f3df7b343f2d4085
Ling Zi Jian <lingzijian@hotmail.com>
2026-02-26 10:57:25 +0800

    feature: added config folder

---
+++ config/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent.parent / ".env"
+    )
+
+    youtube_api_key: str
+    channels: list[str] = [
+        "@TwicebakedJake",
+        "@okJLUV",
+    ]
+    max_videos_per_channel: int = 1
+    language: str = "en"
+
+
+settings = Settings()
